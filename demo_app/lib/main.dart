7d5d063e55c697bbb1f6d7a3e2ac0b3666a27007
Ram <ram2386@gmail.com>
2023-03-03 20:24:49 +0530

    Added the collection view.

--- demo_app/lib/main.dart
+++ demo_app/lib/main.dart
@@ -8,27 +8,64 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Demo App"),
+          title: const Text('ESPN CrickInfo'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.doorbell_rounded),
+              tooltip: 'Show Snackbar',
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('This is a snackbar')));
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.add_alarm_rounded),
+              tooltip: 'Show Snackbar',
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('This is a snackbar')));
+              },
+            )
+          ],
         ),
-        body: Container(
-          color: Colors.lightBlueAccent,
-          height: 220,
-          width: window.physicalSize.width,
-          child: MatchReportWidget(),
-          padding: EdgeInsets.all(20),
-        ),
+        body: GridView.count(
+            crossAxisCount: 1,
+            mainAxisSpacing: double.minPositive,
+            childAspectRatio: 2,
+            children: [
+              Container(
+                color: Colors.lightBlueAccent,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: const [
+                    SizedBox(
+                      width: 300,
+                      child: MatchReportWidget(),
+                    ),
+                    SizedBox(
+                      width: 300,
+                      child: MatchReportWidget(),
+                    ),
+                    SizedBox(
+                      width: 300,
+                      child: MatchReportWidget(),
+                    ),
+                  ],
+                ),
+              ),
+            ]),
       ),
     );
   }

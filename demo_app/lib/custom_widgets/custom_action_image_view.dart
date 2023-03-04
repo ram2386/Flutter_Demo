7ea061ffab9a93bd07b18a1192941da67fe9f0d0
Shashank Pali <>
2023-03-04 22:15:16 +0530

    added action image view

---
+++ demo_app/lib/custom_widgets/custom_action_image_view.dart
@@ -0,0 +1,41 @@
+import 'package:demo_app/constants.dart';
+import 'package:flutter/material.dart';
+
+class CustomActionImageView extends StatelessWidget {
+  final String imageUrl;
+  final BorderRadius borderRadius;
+  final bool showActionButton;
+  final double? width;
+  final double? height;
+
+  CustomActionImageView(
+      {required this.imageUrl,
+      this.borderRadius = BorderRadius.zero,
+      this.showActionButton = false,
+      this.width,
+      this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        ClipRRect(
+          borderRadius: borderRadius,
+          child: Image.network(
+            imageUrl,
+            width: width,
+            height: height,
+            fit: BoxFit.cover,
+          ),
+        ),
+        if (showActionButton)
+          Icon(
+            Icons.play_circle_fill,
+            size: 64,
+            color: Colors.black.withOpacity(0.5),
+          ),
+      ],
+    );
+  }
+}

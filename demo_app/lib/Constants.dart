4ea0a702ef01069827a5fc36989ce16a76dd47f1
Shashank Pali <>
2023-03-04 13:06:49 +0530

    added constant file

---
+++ demo_app/lib/Constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/painting.dart';
+
+const kParagraphFontWeight = FontWeight.w400;
+const kTitleFontWeight = FontWeight.bold;
+
+const kFontColor = Color(0xff6C6D6F);
+const kHighlightedFontColor = Color(0xff2B2C2D);
+const kDividerColor = Color(0xffB8B8B8);
+const kAccentColor = Color(0xff0298DC);
+const kPrimaryColor = Color(0xff03A9F5);
